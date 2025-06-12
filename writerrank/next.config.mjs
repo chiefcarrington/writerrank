d1e794f1d65b3c8bfac563a062e0f923e50a5ce7
chiefcarrington <carrington.tatum@gmail.com>
2025-06-12 10:59:37 -0500

    corrected config filetype to fit downgraded next

---
+++ writerrank/next.config.mjs
@@ -0,0 +1,11 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your Next.js config options here
+  // For now, it's likely empty, which is fine.
+  // Example:
+  // reactStrictMode: true,
+};
+
+export default nextConfig;
